581d33f053752211c6ef8b2298370fecd6ebf927
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2025-08-16 12:00:55 +0700

    integrate insight and event, use proper image

--- src/features/landing/types/index.ts
+++ src/features/landing/types/index.ts
@@ -56,7 +56,7 @@ export interface FeaturedCaseStudy {
   title: string;
   content: RichTextContent;
   quote: RichTextContent;
-  image?: ImageWithAlt;
+  mainImage?: ImageWithAlt;
 }
 
 // Featured insight type
@@ -65,7 +65,7 @@ export interface FeaturedInsight {
   title: string;
   content: RichTextContent;
   quote: RichTextContent;
-  image?: ImageWithAlt;
+  mainImage?: ImageWithAlt;
 }
 
 // Featured event type
