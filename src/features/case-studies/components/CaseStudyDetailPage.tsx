8f0f4df79f2420fde70e2c6866364c22654b16a4
Ari Nugraha <nugrahaa878@gmail.com>
2025-08-20 06:49:34 +0700

    add additional image

--- src/features/case-studies/components/CaseStudyDetailPage.tsx
+++ src/features/case-studies/components/CaseStudyDetailPage.tsx
@@ -186,6 +186,30 @@ const CaseStudyDetailPage: React.FC<CaseStudyDetailPageProps> = ({ documentId })
             >
               {renderRichTextContent(caseStudy.content)}
             </motion.div>
+
+            {/* Additional Image */}
+            {caseStudy.image && (
+              <motion.div
+                className="mt-12 flex flex-col items-center"
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 0.7 }}
+              >
+                <div className="relative w-2/4 h-80">
+                  <Image
+                    src={caseStudy.image.image.url}
+                    alt={caseStudy.image.alternativeText || caseStudy.title}
+                    fill
+                    className="object-cover object-center"
+                  />
+                </div>
+                {caseStudy.image.alternativeText && (
+                  <div className="mt-4 text-center text-gray-300 text-sm italic">
+                    {caseStudy.image.alternativeText}
+                  </div>
+                )}
+              </motion.div>
+            )}
           </div>
         </main>
       </div>
