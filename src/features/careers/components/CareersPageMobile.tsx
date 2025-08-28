5f914cc3556daa1b36745c1a16fa37ac65ac6b1a
Ari Nugraha <nugrahaa878@gmail.com>
2025-08-28 09:18:52 +0700

    add location

--- src/features/careers/components/CareersPageMobile.tsx
+++ src/features/careers/components/CareersPageMobile.tsx
@@ -43,6 +43,21 @@ const JobAccordionItem: React.FC<{
             <p className="text-white mt-1">
               {job.level.toUpperCase()}
             </p>
+            <p className="text-white/80 text-sm mt-1 flex items-center gap-1">
+              <svg
+                className="w-3 h-3"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              {job.location}
+            </p>
           </div>
         </div>
         <motion.div
